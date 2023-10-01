d8af920d8006865ec952e0170cc4c8d58e97f570
danbin <123839021+danbin@users.noreply.github.com>
2023-10-01 00:54:27 -0400

    Update Map.cpp

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -4,34 +4,82 @@
 #include <vector>
 #include <tuple>
 #include <exception>
+#include <sstream>
 #include "Map.h"
 
 using namespace std;
 
- Territory::Territory()
+Map::Map()
 {
-    this->territoryIdentity = "";
+
+}
+
+Map::Map(const Map& mObj)
+{
+    this->continent = mObj.continent;
+    this->territory = mObj.territory;
+}
+
+Map::Map(vector <Continent*> inContinent, vector <Territory*> inTerritory)
+{
+    this->continent = inContinent;
+    this->territory = inTerritory;
+}
+
+vector <Continent*> Map::getContinent()
+{
+    return this->continent;
+}
+
+vector <Territory*> Map::getTerritory()   
+{
+    return this->territory;
+}
+
+void Map::setContinent(vector <Continent*> inContinent)
+{
+    this->continent = inContinent;
+}  
+
+void Map::setTerritory(vector <Territory*> inTerritory)
+{
+    this->territory = inTerritory;
+}
+   
+Territory::Territory()
+{
+    this->territoryName = "";
     this->x = 0;
     this->y = 0;
-    this->continentOfTerritory = nullptr;
-    this->armiesQuantity = 0;
-    this->territoryHolder = nullptr;
+    this->continentName = "";
+    this->numberOfArmies = 0;
+    this->owner = nullptr;
 }
 
-Territory::Territory(string inTerritoryIdentity, int inX, int inY, Continent* inContinentOfTerritory, vector <Territory*> inAdjacentTerritories, int inArmyQuantity, Player* inTerritoryHolder)
+Territory::Territory(const Territory& tObj)
 {
-    this->territoryIdentity = inTerritoryIdentity;
+    this->territoryName = tObj.territoryName;
+    this->x = tObj.x;
+    this->y = tObj.y;
+    this->continentName = tObj.continentName;
+    this->numberOfArmies = tObj.numberOfArmies;
+    this->owner = tObj.owner;
+}
+
+Territory::Territory(string inTerritoryName, int inX, int inY, string inContinentName, vector <Territory*> inAdjacentTerritories, int inArmyQuantity, Player* inTerritoryHolder)
+{
+    this->territoryName = inTerritoryName;
     this->x = inX;
     this->y = inY;
-    this->continentOfTerritory = inContinentOfTerritory;
+    this->continentName = inContinentName;
     this->adjacentTerritories = inAdjacentTerritories;
-    this->armiesQuantity = inArmyQuantity;
-    this->territoryHolder = inTerritoryHolder;
+    this->numberOfArmies = inArmyQuantity;
+    this->owner = inTerritoryHolder;
 }
 
-string Territory::getTerritoryIdentity()
+string Territory::getTerritoryName()
 {
-    return this->territoryIdentity;
+    return this->territoryName;
 }
 
 int Territory::getX()
@@ -44,9 +92,9 @@ int Territory::getY()
     return this->y;
 }
 
-Continent* Territory::getContinentOfTerritory()
+string Territory::getContinentName()
 {
-    return this->continentOfTerritory;
+    return this->continentName;
 }
 
 vector <Territory*> Territory::getAdjacentTerritories()
@@ -54,9 +102,9 @@ vector <Territory*> Territory::getAdjacentTerritories()
     return this->adjacentTerritories;
 }
 
-void Territory::setTerritoryIdentity(string inTerritoryIdentity)
+void Territory::setTerritoryName(string inTerritoryName)
 {
-    this->territoryIdentity = inTerritoryIdentity;
+    this->territoryName = inTerritoryName;
 }
 
 void Territory::setX(int inX)
@@ -69,9 +117,9 @@ void Territory::setY(int inY)
     this->y = inY;
 }
 
-void Territory::setContinentOfTerritory(Continent* inContinentOfTerritory)
+void Territory::setContinentIndex(string inContinentName)
 {
-    this->continentOfTerritory = inContinentOfTerritory;
+    this->continentName = inContinentName;
 }
 void Territory::setAdjaccentTerritories(vector <Territory*> inAdjacentTerritories)
 {
@@ -81,13 +129,26 @@ void Territory::setAdjaccentTerritories(vector <Territory*> inAdjacentTerritorie
 Continent::Continent()
 {
     this->continentIdentity = "";
-    this->continentID = 0; 
+    this->numberOfTerritories = 0; 
 }
 
-Continent::Continent(string inContinentIdentity, int inContinentID, vector <Territory*> inTerritories)
+Continent::Continent(const Continent& cObj)
+{
+    this->continentIdentity = cObj.continentIdentity;
+    this->numberOfTerritories = cObj.numberOfTerritories; 
+    this->Territories = cObj.Territories;
+}
+
+Continent::Continent(string inContinentIdentity, int inNumberOfTerritories)
 {
     this ->continentIdentity = inContinentIdentity;
-    this ->continentID = inContinentID;
+    this ->numberOfTerritories = inNumberOfTerritories;
+}
+
+Continent::Continent(string inContinentIdentity, int inNumberOfTerritories, vector <Territory*> inTerritories)
+{
+    this ->continentIdentity = inContinentIdentity;
+    this ->numberOfTerritories = inNumberOfTerritories;
     this ->Territories = inTerritories;
 }
     
@@ -96,9 +157,9 @@ string Continent::getContinentIdentity()
     return this->continentIdentity;
 }
 
-int Continent::getContinentID()
+int Continent::getNumberOfTerritories()
 {
-    return this->continentID;
+    return this->numberOfTerritories;
 }
 
 vector <Territory*> Continent::getTerritories()
@@ -111,13 +172,131 @@ void Continent::setContinentIdentity(string inContinentIdentity)
     this->continentIdentity = inContinentIdentity;
 }
 
-void Continent::setContinentID(int inContinentID)
+void Continent::setNumberOfTerritories(int inContinentID)
 {
-    this->continentID = inContinentID;
+    this->numberOfTerritories = inContinentID;
 }
 
 void Continent::setTerritories(vector <Territory*> inTerritories)
 {
     this->Territories = inTerritories;
+}
+
+string mapLoader::getContinentNameFromLine(string inStr)
+{
+        return inStr.substr(0, inStr.find("="));
+}
+
+int mapLoader::getNumberOfTerritoriesFromLine(string inStr)
+{
+     return stoi(inStr.substr(inStr.find("=") + 1,inStr.length()-1));     
+}
+
+
+string mapLoader::getContinentIDFromTerritory(string inStr)
+{
+    
+    vector<string> tokens;
+    //stringstream lineStringStream(inStr);
+    string token;
+    string tmpStr;
+    tmpStr = inStr;
+    int offset = 0;
+    //cout << inStr << endl;
+    int i = 0;
+    //for (int i = 0; i < 6; i++)
+    while (tmpStr.length() > 1)
+    {
+        token = tmpStr.substr(offset, tmpStr.find(','));
+        //cout << token << endl;
+        tokens.push_back(token);
+        i++;
+        tmpStr =  tmpStr.substr(tmpStr.find(',') +1, tmpStr.length() - 1 );
+        //cout << tmpStr.length() << endl;
+    }
+
+    return tokens[3];
+    
+    //token = inStr.substr(0, inStr.find(','));
+    //cout << inStr.substr(0, inStr.find(',')) << endl;
+    //cout << token << endl;
+    //tmpStr =  inStr.substr(inStr.find(',') +1, inStr.length() - 1 );
+    //token = tmpStr.substr(0, tmpStr.find(','));
+    //cout << token << endl;
+    // cout << tmpStr << endl;
+    //cout << "AAA";
+/*
+    while (getline(lineStringStream, token, ','))
+    {
+        cout << token << endl;
+        tokens.push_back(token);
+    }
+*/
+}
+
+
+
+Map* mapLoader::createMapFromConquestFile(string inFileName)
+{
+    vector <Continent*> mapContinents;
+    vector <Territory*> mapTerritories;
+
+    bool continentSectionFound = false;
+    bool territorySectionFound = false;
+    string read;
+    ifstream inputfilestream;
+    inputfilestream.open(inFileName);
+   
+    while (!inputfilestream.eof())
+    {
+        getline(inputfilestream, read);
+        if(read == "[Continents]")
+        {
+            continentSectionFound = true;
+        }
+        else if (continentSectionFound)
+        {
+            if (read =="")
+            {
+                continentSectionFound = false;;
+            }
+            else
+            {
+                //cout << mapLoader::getContinentNameFromLine(read) << endl;
+                //cout << mapLoader::getNumberOfTerritoriesFromLine(read) << endl;
+                Continent* c = new Continent(mapLoader::getContinentNameFromLine(read), mapLoader::getNumberOfTerritoriesFromLine(read));
+                mapContinents.push_back(c);
+            }
+            
+        }
+        else if(read == "[Territories]")
+        {
+            territorySectionFound = true;
+        }
+        else if(territorySectionFound)
+        {
+            if (read =="")
+            {
+                //territorySectionFound = false;
+                continue;
+            }
+            else
+            {
+                cout << mapLoader::getContinentIDFromTerritory(read) << endl;
+            }
+        }
+    }
+    
+    
+    inputfilestream.close();
+    /*
+    for (int i = 0; i < mapContinents.size(); i++) 
+    {
+         cout << mapContinents[i]->getContinentIdentity() << endl;
+    }
+    */
+
+   
+
 }
 
