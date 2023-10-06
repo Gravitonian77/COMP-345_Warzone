e7b2d6230e746cd1190fbb5a612390417305478d
Gravitonian77 <122951929+Gravitonian77@users.noreply.github.com>
2023-10-06 18:43:45 -0500

    Update GameEngine.cpp

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include "GameEngine.h"
-#include "Map.h"
 
+//Implementation of State class
+
+// Default constructor
 State::State() = default;
+
+// Parameterized constructor initializing stateName with provided state
 State::State(std::string& state) {
 	this->stateName = &state;
 }
 
+// Getter method to retrieve the state name
 string* State::getStateName() const{
 	return stateName;
 }
 
+// Copy constructor to create a new State object with the same stateName as anotherState
 State::State(State &anotherState) {
 	this->stateName = anotherState.stateName;
 }
 
+// Overloaded assignment operator to assign the stateName of one State object to another
 State &State::operator=(const State &state) {
 	if(this != &state)
 		this->stateName = state.stateName;
@@ -22,6 +29,7 @@ State &State::operator=(const State &state) {
 	return *this;
 }
 
+// Overloaded stream insertion operator to output the stateName
 ostream &operator<<(ostream& out, const State &state) {
 
 		if((state.getStateName()) != nullptr) {
@@ -32,56 +40,36 @@ ostream &operator<<(ostream& out, const State &state) {
 	return out;
 }
 
+// Method to set the current state of the engine and display a message
 void State::onEnter(GameEngine *engine) {
 	engine->setCurrentState(this);
 	cout << "State changed\n";
 }
 
+// Method to reset the current state of the engine and display a message
 void State::onExit(GameEngine *engine) {
 	engine->setCurrentState(nullptr);
 	cout << "Changing states...\n";
 }
 
 
-Transition::Transition() = default;
-Transition::Transition(State *fromState, State *toState, std::string *command) {
-	this->from = fromState;
-	this->to = toState;
-	this->command = command;
-}
+//Implementation of GameEngine class
 
-Transition::Transition(const Transition &transition) {
-	this->from = transition.from;
-	this->to = transition.to;
-	this->command = transition.command;
-}
-
-Transition &Transition::operator=(const Transition &transition) {
-	if(this != &transition){
-		this->to = transition.to;
-		this->from = transition.from;
-		this->command = transition.command;
-	}
-	return *this;
-}
-
-ostream &operator<<(ostream& out, const Transition& transition){
-	out << "Transitioning from: " << transition.from << ", to: " << transition.to <<endl;
-	return out;
-}
-
-
+// Default constructor
 GameEngine::GameEngine() = default;
+
+// Parameterized constructor initializing currentState and transitions
 GameEngine::GameEngine(State *initialState, std::pair<State*, State*> transitions) {
 	this->currentState = initialState;
-	//this ->transitions = transitions;
 }
 
+// Copy constructor to create a new GameEngine object with the same currentState and transitions as game
 GameEngine::GameEngine(const GameEngine &game) {
 	this->currentState = game.currentState;
 	this->transitions = game.transitions;
 }
 
+// Overloaded assignment operator to assign the currentState and transitions of one GameEngine object to another
 GameEngine &GameEngine::operator=(const GameEngine &game) {
 	if(this!= &game){
 		this->currentState = game.currentState;
@@ -90,50 +78,50 @@ GameEngine &GameEngine::operator=(const GameEngine &game) {
 	return *this;
 }
 
-/*ostream &operator<<(ostream& out, const GameEngine& game){
-	out << "State: " << game.getCurrentState() << " Transition: " << game.getTransition();
-	return out;
-}*/
+// Overloaded stream insertion operator to output the current state and transitions
+ostream &operator<<(ostream& out, const GameEngine& game){
 
-void GameEngine::setCurrentState(State *state) {
-	this->currentState = state;
-	//this->currentState->stateName = state->stateName;
+	string s = *game.getCurrentState()->getStateName();
+	out << "State: " << s << " Transition from: " << game.getTransition().at(s).first << " to: " << game.getTransition().at(s).second;
+	return out;
+
 }
 
+// Method to set the current state of the engine
+void GameEngine::setCurrentState(State *state) {
+	this->currentState = state;
+}
+
+// Getter method to retrieve the current state of the engine
 State *GameEngine::getCurrentState() const {
 	return this->currentState;
 }
 
-void GameEngine::changeState(string& command) {
-	//change current state if newState is not the final state
-
-	if((transitions.find(command) != transitions.end()) && (transitions.find(command)->second.second->getStateName())) {
-		currentState->onExit(this);
-		cout<< "debug: " << *transitions[command].second;//to //A -> B ; B
-		setCurrentState(transitions[command].second);
-		currentState->onEnter(this);
-		cout <<  *currentState;
-	}
-
-}
-
+// Method to add a transition from one state to another based on a command
 void GameEngine::addTransition(State *from, State *to, string command) {
 	transitions[command].first = from;
 	transitions[command].second = to;
 }
 
+// Getter method to retrieve the transitions of the engine
 map<std::string, std::pair<State*, State*>> GameEngine::getTransition() const {
 	return transitions;
 }
 
-void GameEngine::setMap(Map *pMap) {
-	this->map = pMap;
+// Method to change the current state of the engine based on a command
+void GameEngine::changeState(string& command) {
+
+	//change current state if newState is not the final state
+	if((transitions.find(command) != transitions.end()) && (transitions.find(command)->second.second->getStateName())) {
+		currentState->onExit(this);
+		setCurrentState(transitions[command].second);
+		currentState->onEnter(this);
+		cout << "\n" <<*currentState;
+	}
+
 }
 
-Map *GameEngine::getMap() {
-	return this->map;
-}
-
+// Method to start the game and accept a command from the user
 void GameEngine::startGame() {
 	string command;
 	std::cout << "Enter a command: ";
@@ -141,64 +129,60 @@ void GameEngine::startGame() {
 	processCommand(command);
 }
 
+// Method to process a command and transition to the appropriate state
 void GameEngine::processCommand(std::string command) {
-	string stateNow = *getCurrentState()->getStateName();
+
+	cout << *currentState;
+	string stateNow = *getCurrentState()->getStateName();   //For checking the current state
+
 	if(transitions.find(command) != transitions.end()){
-		//currentState = new State(command);
 
-		if(command == "loadmap" && ((stateNow == "start") || (stateNow == "load_map"))){
-
-			changeState(command);
-			//setMap(mapLoader::createMapFromConquestFile(""));
-
-		}else if(command == "validatemap" && (stateNow == "load_map")){
+		if(stateNow == "start" && command == "loadmap"){
 
 			changeState(command);
 
-		}else if(command == "addplayer" && ((stateNow == "map_validated") || (stateNow == "players_added"))){
+		}else if(stateNow == "load_map" && (command == "validatemap" || command == "loadmap")){
 
 			changeState(command);
 
-		}else if(command == "assigncountries" && (stateNow == "players_added")){
-			cout << "previous state = " << stateNow << endl;
-			changeState(command);
-			stateNow = *getCurrentState()->getStateName();
-			cout << "current state = " << stateNow <<endl;
-		}else if(command == "issueorder" && ((stateNow == "assign_ref") || (stateNow == "issue_orders"))){
+		}else if(stateNow == "map_validated" && command == "addplayer"){
 
 			changeState(command);
 
-		}else if(command == "endissueorders" && (stateNow == "issue_orders")){
+		}else if(stateNow == "players_added" && (command == "addplayer" || command == "assigncountries")){
 
 			changeState(command);
 
-		}else if(command == "execorder" && (stateNow == "execute_orders")){
+		}else if(stateNow == "assign_ref" && command == "issueorder"){
 
 			changeState(command);
 
-		}else if(command == "endexecorders" && (stateNow == "execute_orders")){
+		}else if(stateNow == "issue_orders" && (command == "issueorder" || command == "endissueorders")){
 
 			changeState(command);
 
-		}else if(command == "win" && (stateNow == "execute_orders")){
+		}else if(stateNow == "execute_orders" && (command == "execorder" || command == "endexecorders") || (command == "win")){
+
+			changeState(command);
+
+		}else if(command == "play" && stateNow == "win"){
 
 			changeState(command);
 
 		}else if(command == "end"){
 
-			changeState(command);
+			exit(0);
 
-		}else if(command == "play" && stateNow == "win"){
-			changeState(command);
 		}else{
 			cout << "This is not a valid transition. Please enter a valid state to transition to. \n";
 		}
 
 	}else
-		cout << "not working";
+		cout << "Invalid input.\n";
 
 }
 
+// Method to create a new state with a given name
 State* GameEngine::newState(std::string stateName) {
 	State * state = new State(stateName);
 	return state;
