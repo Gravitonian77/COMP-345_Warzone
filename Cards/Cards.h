53f65ba258ebaef468a0f2cfe2b27a63dc741e2c
zaheerqur <91912365+zaheerqur@users.noreply.github.com>
2023-10-06 18:59:42 -0400

    Update Cards.h

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -1,47 +1,51 @@
 #ifndef CARDS_H
 #define CARDS_H
 
-#include <string>
+#pragma once
+
 #include <vector>
-#include <algorithm>
-#include <random>
-#include <chrono>
-#include <iostream>
+#include <string>
+
+enum class CardType {
+    BOMB, REINFORCEMENT, BLOCKADE, AIRLIFT, DIPLOMACY
+};
 
 class Card {
+private:
+    CardType* type;
 public:
-    enum class CardType { BOMB,
-            REINFORCEMENT,
-            BLOCKADE,
-            AIRLIFT,
-            DIPLOMACY };
-
     explicit Card(CardType type);
     Card(const Card& other);
     Card& operator=(const Card& other);
     ~Card();
-
-    [[nodiscard]] CardType getType() const;
-    void play();  // Play the card and add an order
-
-private:
-    CardType* type;  // Use a pointer for card type
+    void play();
+    friend std::ostream& operator<<(std::ostream& os, const Card& card);
 };
 
 class Deck {
+private:
+    std::vector<Card*> cards;
 public:
     Deck();
     Deck(const Deck& other);
     Deck& operator=(const Deck& other);
     ~Deck();
-
-    void shuffle();
-    Card* draw();  // Use a pointer for the drawn card
-
-private:
-    std::vector<Card*> cards;  // Use pointers for cards in the deck
+    Card* draw();
+    void returnCard(Card* card);
 };
 
-void testCards();
+class Hand {
+private:
+    std::vector<Card*> cards;
+public:
+    Hand();
+    Hand(const Hand& other);
+    Hand& operator=(const Hand& other);
+    ~Hand();
+    void addCard(Card* card);
+    void playAll(Deck& deck);
+};
+
+void testCards();  // Function declaration added here
 
 #endif
