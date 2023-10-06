108e953c386b5133cedff449b5a399c441782974
zaheerqur <91912365+zaheerqur@users.noreply.github.com>
2023-10-05 20:37:25 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,1 +1,24 @@
+// MainDriver.cpp
+#include "Cards.h"
 
+// Function to test the Card classes
+void testCards() {
+    Deck deck;
+    Hand hand;
+
+    // Fill the hand by drawing cards from the deck
+    for (int i = 0; i < 5; ++i) {
+        Card card = deck.draw();
+        hand.addCard(card);
+    }
+
+    // Play all cards in the hand and return them to the deck
+    hand.playAll(deck);
+
+    std::cout << "Cards remaining in the deck: " << deck.size() << std::endl;
+}
+
+int main() {
+    testCards();
+    return 0;
+}
