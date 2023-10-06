3a81d08284d0ab8d22a6e30fb2868fe8a83a164c
zaheerqur <91912365+zaheerqur@users.noreply.github.com>
2023-10-06 00:30:35 -0400

    Update CardsDriver.cpp

--- Cards/CardsDriver.cpp
+++ Cards/CardsDriver.cpp
@@ -1,20 +1,6 @@
 // CardsDriver.cpp
 #include "Cards.h"
 
-void testCards() {
-    Deck deck;
-    Hand hand;
-
-    // Draw cards from the deck and add them to the hand.
-    for (int i = 0; i < 5; ++i) {
-        Card drawnCard = deck.draw();
-        hand.addCard(drawnCard);
-    }
-
-    // Play all cards in the hand.
-    hand.playAll();
-}
-
 int main() {
     testCards();
     return 0;
