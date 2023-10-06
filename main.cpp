a18b56bddee2cf3cfb75e630fd5e5bab28510111
zaheerqur <91912365+zaheerqur@users.noreply.github.com>
2023-10-06 00:38:53 -0400

    Updated main.cpp (Added testCards function to Cards.cpp)

--- main.cpp
+++ main.cpp
@@ -59,21 +59,6 @@ void testPlayers() {
     delete p1;
     delete p2;
 }
-// Function to test the Card classes
-void testCards() {
-    Deck deck;
-    Hand hand;
-
-    // Draw cards from the deck and add them to the hand.
-    for (int i = 0; i < 5; ++i) {
-        Card drawnCard = deck.draw();
-        hand.addCard(drawnCard);
-    }
-
-    // Play all cards in the hand.
-    hand.playAll();
-}
-
 int main() {
     testPlayers();
     testCards();
