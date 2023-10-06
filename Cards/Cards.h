53b9ffd7272941c27eaca5cdd082c178b72b0ec5
zaheerqur <91912365+zaheerqur@users.noreply.github.com>
2023-10-06 00:29:30 -0400

    Update Cards.h

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -1,63 +1,47 @@
 #ifndef CARDS_H
 #define CARDS_H
 
-#include <vector>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <iostream>
 
 class Card {
 public:
-    enum CardType {
-        BOMB,
-        REINFORCEMENT,
-        BLOCKADE,
-        AIRLIFT,
-        DIPLOMACY
-    };
+    enum class CardType { BOMB,
+            REINFORCEMENT,
+            BLOCKADE,
+            AIRLIFT,
+            DIPLOMACY };
 
-    // Constructor to initialize a Card object with a specific type.
-    Card(CardType type);
+    explicit Card(CardType type);
+    Card(const Card& other);
+    Card& operator=(const Card& other);
+    ~Card();
 
-    // Method to simulate playing a card.
-    void play();
-
-    // Getter method to retrieve the card's type.
-    CardType getType() const;
-
-    // Getter method to retrieve the card's type as a string.
-    std::string getTypeAsString() const;
+    [[nodiscard]] CardType getType() const;
+    void play();  // Play the card and add an order
 
 private:
-    CardType type_;
+    CardType* type;  // Use a pointer for card type
 };
 
 class Deck {
 public:
-    // Constructor to initialize a Deck object.
     Deck();
+    Deck(const Deck& other);
+    Deck& operator=(const Deck& other);
+    ~Deck();
 
-    // Method to initialize and shuffle the deck.
-    void initializeDeck();
-
-    // Method to draw a card from the deck.
-    Card draw();
-
-private:
-    std::vector<Card> cards_;
-
-    // Helper method to shuffle the deck.
-    void shuffleDeck();
-};
-
-class Hand {
-public:
-    // Method to add a card to the hand.
-    void addCard(const Card& card);
-
-    // Method to simulate playing all cards in the hand.
-    void playAll();
+    void shuffle();
+    Card* draw();  // Use a pointer for the drawn card
 
 private:
-    std::vector<Card> cards_;
+    std::vector<Card*> cards;  // Use pointers for cards in the deck
 };
 
-#endif // CARDS_H
+void testCards();
+
+#endif
