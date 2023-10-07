decf0c6f4ad790820bd7a4298db230e0f290666a
Saqib <114625167+saqpasse@users.noreply.github.com>
2023-10-06 20:55:17 -0400

    Update Player.cpp

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -150,5 +150,77 @@ void Player::issueOrder(const string& order) {
     }
 }
 
+void testPlayers() {
+    // Create the map 
+    Map* gameMap = mapLoader::createMapFromConquestFile("Canada.map");
 
+    // Players
+    Player* p1 = new Player();
+    p1->addTerritory(gameMap->getContinent()[0]->getTerritories()[0]); // 1,548,116,Northern Islands,2,12
+    p1->addTerritory(gameMap->getContinent()[0]->getTerritories()[1]); // 2,510,124,Northern Islands,1,3
 
+    Player* p2 = new Player();
+    p2->addTerritory(gameMap->getContinent()[0]->getTerritories()[2]); // 3,487,139,Northern Islands,2,4
+    p2->addTerritory(gameMap->getContinent()[0]->getTerritories()[3]); // 4,463,136,Northern Islands,3,5
+
+    cout << endl << *p1 << endl;
+    cout << *p2 << endl;
+
+    // P1 territories to attack
+    cout << "Player " << p1->getPlayerNumber() << " can attack territories: ";
+    vector<Territory*> toAttackp1 = p1->ToAttack();
+    for (Territory* t : toAttackp1) {
+        cout << t->getTerritoryName() << " ";
+    }
+    cout << endl;
+
+    // P1 territories to defend
+    cout << "Player " << p1->getPlayerNumber() << " can defend territories: ";
+    vector<Territory*> toDefendp1 = p1->ToDefend();
+    for (Territory* t : toDefendp1) {
+        cout << t->getTerritoryName() << " ";
+    }
+    cout << endl;
+
+    // P2 territories to attack
+    cout << "Player " << p2->getPlayerNumber() << " can attack territories: ";
+    vector<Territory*> toAttackp2 = p2->ToAttack();
+    for (Territory* t : toAttackp2) {
+        cout << t->getTerritoryName() << " ";
+    }
+    cout << endl;
+
+    // P2 territories to defend
+    cout << "Player " << p2->getPlayerNumber() << " can defend territories: ";
+    vector<Territory*> toDefendp2 = p2->ToDefend();
+    for (Territory* t : toDefendp2) {
+        cout << t->getTerritoryName() << " ";
+    }
+    cout << endl << endl;
+
+    // Add orders to players
+    p1->issueOrder("Attack Territory  12 3");   // valid
+    p1->issueOrder("Defend Territory 2");       // valid
+    p1->issueOrder("Move to Territory  12 3");  // invalid
+
+    p2->issueOrder("Attack territory 2 5");     // valid
+    p2->issueOrder("Defend Territory 3 4");     // valid
+    p2->issueOrder("Move to Territory  2 5");  // invalid
+    
+    // Display orders
+    cout << "\nPlayer " << p1->getPlayerNumber() << " has orders: ";
+    for (const string& order : p1->getOrders()) {
+        cout << order << ", ";
+    }
+    cout << endl;
+
+    cout << "Player " << p2->getPlayerNumber() << " has orders: ";
+    for (const string& order : p2->getOrders()) {
+        cout << order << ", ";
+    }
+    cout << endl;
+
+    // Release memory
+    delete p1;
+    delete p2;
+}
