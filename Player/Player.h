5183aabf126b50ab5c56a2f92a3ac77c67badd80
Saqib <114625167+saqpasse@users.noreply.github.com>
2023-11-07 21:30:41 -0500

    Update Player.h For A2

--- Player/Player.h
+++ Player/Player.h
@@ -1,28 +1,36 @@
 #pragma once
 
 #include "Map.h"
+#include "Cards.h"
+#include "Orders.h"
+#include "GameEngine.h"
+
 #include <vector>
-#include <string>
 #include <list>
 #include <iostream>
 
 using namespace std;
 
+// Forward declaration
 class Territory;
+class Hand;
+class Orders;
+class OrdersList;
 
 class Player {
 private:
     // Attributes
     static int nextPlayerNumber;
-    int* playerNumber;
-    vector<Territory*> myTerritories;
-    list<string> myCards;    
-    list<string> myOrders;   
+    int playerNumber;
+    vector<Territory*> myTerritories; 
+	OrdersList* myOrdersList;
+	int reinforcementPool; 
+	Hand* myHand; 
 
 public:
     // Constructors
     Player();
-    Player(vector<Territory*> territories, const list<string>& cards, const list<string>& orders); // Updated constructor
+    Player(vector<Territory*> territories, OrdersList* ordersList, Hand* hand); // Updated constructor
     Player(const Player& player);
 
     // Destructor
@@ -30,12 +38,20 @@ public:
 
     // Accessors
     int getPlayerNumber() const;
+    int getReinforcementPool() const;
     vector<Territory*> getMyTerritories() const;
-    list<string> getCards() const;
-    list<string> getOrders() const;
+    OrdersList* getMyOrdersList() const;
+    Hand* getMyHand() const; 
+	
+    // Mutators
+    void setReinforcementPool(int pool);
+    void setMyTerritories(vector<Territory*>& newTerritories);
+    void setMyOrdersList(OrdersList* newOrdersList);
+    void setMyHand(Hand* newHand);
 
-    // Add territory to the player's list
+    // Add/Remove territory to the player's list
     void addTerritory(Territory* territory);
+    void removeTerritory(Territory* territory);
 
     // Assignment operator
     Player& operator=(const Player& other);
@@ -46,5 +62,5 @@ public:
     // Methods Required
     vector<Territory*> ToDefend();
     vector<Territory*> ToAttack();
-    void issueOrder(const string& order); // Updated method to accept a string
+    void issueOrder(Order* order); 
 };
